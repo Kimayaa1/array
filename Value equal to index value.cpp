47b7925d2c0dc03b2b14c7410aa77980353d2c0f
Kimaya Gupta <91895421+Kimayaa1@users.noreply.github.com>
2024-11-24 20:26:27 +0530

    Finding Value equal to index value

---
+++ Value equal to index value.cpp
@@ -0,0 +1,13 @@
+//User function template for C++
+class Solution {
+public:
+    vector<int> valueEqualToIndex(int arr[], int n) {
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == i + 1) {
+                result.push_back(arr[i]);
+            }
+        }
+        return result;
+    }
+};
